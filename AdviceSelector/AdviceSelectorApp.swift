c65063dd132efcf25e0f8e7ec934a6734181c766
Madison Dellamea <mdellamea@lcs.on.ca>
2022-11-04 21:11:10 -0400

    Initial Commit

---
+++ AdviceSelector/AdviceSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdviceSelectorApp.swift
+//  AdviceSelector
+//
+//  Created by Madison Dellamea on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct AdviceSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
